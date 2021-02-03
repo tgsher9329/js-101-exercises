fae8fc8ff37b38965e3e148fa4aa4a6cc9458968
Tucker Sheram <tgsher9329@outlook.com>
2021-02-03 15:14:29 -0500

    updating after answering the questions

--- exercises/level1.js
+++ exercises/level1.js
@@ -1,34 +1,43 @@
 function lvl1exercise1 () {
+  return answer = 10
   // Declare a variable without instantiating it and return it!
 
 }
 
 function lvl1exercise2 () {
+  return answer = 10
   // Declare and instantiate a number and return it
 
 }
 
 function lvl1exercise3 () {
+  return answer = 10.01
   // Declare and instantiate a floating point number that is not a whole number and return it
 
 }
 
 function lvl1exercise4 () {
+  return answer = ("Hello World!")
   // Declare and instantiate a string "Hello World!" and return it
 
 }
 
 function lvl1exercise5 () {
+  return answer = ["Hello World!", 4]
   // Declare and instantiate an array containing the string "Hello World!" and the number 4 and return it
 
 }
 
 function lvl1exercise6 () {
+  answer = {key1 : "Hello World!" , key2 : 4}
+  return answer
   // Declare and instantiate an object containing the key-value pairs key1 -> "Hello World!" and key2 -> 4, and return it
 
 }
 
 function lvl1exercise7 () {
+  return answer = false
+
   // Declare and instantiate a boolean value 'false' and return it
 
 }
